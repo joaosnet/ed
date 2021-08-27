a3860bc93f7347f743843705241622f26c2b38ba
joaosnet <joaodacruznatividade@gmail.com>
2021-08-27 19:05:07 -0300

    Já estou fazendo as questões

--- Lista03/src/arranjo/Arranjo.java
+++ Lista03/src/arranjo/Arranjo.java
@@ -3,7 +3,6 @@ package arranjo;
 public class Arranjo<T> {
 
     private T[] Data;
-    
     @SuppressWarnings("unchecked")
     public Arranjo(int tam) {
         this.Data = (T[]) new Object[tam];
@@ -24,5 +23,28 @@ public class Arranjo<T> {
         }
         System.out.println("]");
     }
-
+    /*
+    3. a. Construa um método para inserir um dado na 
+    última posição não ocupada do arranjo. 
+    O dado é parâmetro do método. 
+    */
+    public void insertLast(T obj){
+        int n = 0;
+        for (int i=0;i<this.Data.length;i++){
+            n=i;
+        }
+        
+        this.Data[n] = obj;
+    }
+    /*
+    3. b. Construa um método para inserir um dado na primeira posição do arranjo. 
+    Movimente os dados pré-existentes preservando seu conteúdo e ordem. 
+    O dado é parâmetro do método. (João)
+    */
+    public void insertFirst(T obj){
+        int n = 0;
+        for (int i=0;i<this.Data.length;i++){
+            n=i;
+        }
+    }
 }
--- Lista03/src/arranjo/TesteArranjo.java
+++ Lista03/src/arranjo/TesteArranjo.java
@@ -1,7 +1,10 @@
 package arranjo;
 
+import java.util.Scanner;
+
 public class TesteArranjo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
 
 		Arranjo<String> arr1 = new Arranjo<String>(5);
 		
@@ -9,8 +12,18 @@ public class TesteArranjo {
         arr1.Set(1,"b");
         System.out.println(arr1.Get(1));
         arr1.Set(2,"c");
+        System.out.println("Qual o último dado a ser inserido?");
+        /*
+    a. Construa um método para inserir um dado na 
+    última posição não ocupada do arranjo. 
+    O dado é parâmetro do método. 
+    */  
+        arr1.insertLast(sc.nextLine());
+        
         arr1.Imprime();
 		arr1.Set(3,"d");
+                
         arr1.Imprime();
+        
 	}   
 }
