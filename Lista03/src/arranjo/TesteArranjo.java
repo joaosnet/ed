48eb2e407d61ebefc55a613729acf2afcae35b8a
joaosnet <joaodacruznatividade@gmail.com>
2021-08-28 22:10:49 -0300

    Update TesteArranjo.java

--- Lista03/src/arranjo/TesteArranjo.java
+++ Lista03/src/arranjo/TesteArranjo.java
@@ -7,52 +7,47 @@ public class TesteArranjo {
         Scanner sc = new Scanner(System.in); 
 
         Arranjo<String> arr1 = new Arranjo<String>(5);
-        
-        System.out.println("Qual o último dado a ser inserido?");
-        /*
+       
+        arr1.Set(0,"a");
+        arr1.Set(1,"b");
+        System.out.println(arr1.Get(1));
+        arr1.Set(2,"c");
+        arr1.Imprime();
+        arr1.Set(3,"d"); 
+        arr1.Imprime();
+         /*
         a. Construa um método para inserir um dado na 
         última posição não ocupada do arranjo. 
         O dado é parâmetro do método. 
         */  
+        System.out.println("\n==== Questão 3.a. ====");
+        System.out.println("Qual o último dado a ser inserido?");
         arr1.insertLast(sc.nextLine());
-        
-        arr1.Set(0,"a");
-        
-        arr1.Set(1,"b");
-        System.out.println(arr1.Get(1));
-        
-        arr1.Set(2,"c");
-        
-        arr1.Imprime();
-        arr1.Set(3,"d");
-                
-        arr1.Imprime();
-        
-        System.out.println("Qual o dado a ser inserido na primeira posição do arranjo?");
         /*
         3. b. Construa um método para inserir um dado na primeira posição do arranjo. 
         Movimente os dados pré-existentes preservando seu conteúdo e ordem. 
         O dado é parâmetro do método.
         */
+        System.out.println("\n==== Questão 3.b. ====");
+        System.out.println("Qual o dado a ser inserido na primeira posição do arranjo?");
         arr1.insertFirst(sc.nextLine());
         arr1.Imprime();
-        
         /*
         3. c. Construa um método para inserir um dado em uma dada posição do arranjo. 
         Preserve o conteúdo e a ordem dos dados subsequentes à posição desejada 
         quando os mover de posição. O dado e a posição desejada são parâmetros 
         do método.
         */
+        System.out.println("\n==== Questão 3.c. ====");
         arr1.add(0, "e");
         arr1.Imprime();
-        
         /*
         3. d. Construa um método para remover e retornar o dado da última posição 
         ocupada do arranjo.     
         */
+        System.out.println("\n==== Questão 3.d. ====");
         System.out.println(arr1.remove());
         arr1.Imprime();
-
         /*
         3. e. Construa um método para remover e retornar o dado da primeira posição 
         do arranjo. Movimente os dados restantes preservando o conteúdo e a ordem 
@@ -63,7 +58,6 @@ public class TesteArranjo {
         System.out.println(arr1.Q3_e());
         System.out.print("Arranjo reordenado: ");
         arr1.Imprime();
-        
         /*
         3. f. Construa um método para remover um dado em uma dada posição do 
         arranjo. Preserve o conteúdo e a ordem dos dados subsequentes à posição 
@@ -74,6 +68,5 @@ public class TesteArranjo {
         arr1.Q3_f(0);
         System.out.print("Arranjo reordenado: ");
         arr1.Imprime();
-
-	}   
+    }   
 }
