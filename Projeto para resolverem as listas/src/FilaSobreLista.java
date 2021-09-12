9a183cb6d261de515995ea3a7d44380b97e4a020
João Neto <87316339+joaosnet@users.noreply.github.com>
2021-09-12 13:37:32 -0300

    FilaSobreLista

---
+++ Projeto para resolverem as listas/src/FilaSobreLista.java
@@ -0,0 +1,28 @@
+
+import fila.Fila;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author João Neto 2
+ */
+public class FilaSobreLista extends Fila{
+    /*
+    5 - Implemente a classe FilaSobreLista. Esta é uma reimplementação da classe 
+    Fila (disponível em nosso repositório) utilizando um objeto da classe 
+    ListaDuplaCauda para armazenar os elementos da Lista. 
+    Implemente os métodos Enqueue(Elemento<T> e), Dequeue(), QueueFull() 
+    e QueueEmpty(). Procure utilizar chamadas aos métodos da classe 
+    ListaDuplaCauda nos métodos que você criar. (João)
+    */
+    
+    
+}
