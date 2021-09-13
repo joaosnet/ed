ea9d3bf932f796da13e6ca68ad07ba8b4d522218
João Neto <87316339+joaosnet@users.noreply.github.com>
2021-09-13 14:44:42 -0300

    terminei

--- Projeto para resolverem as listas/src/lista/ListaOrdenada.java
+++ Projeto para resolverem as listas/src/lista/ListaOrdenada.java
@@ -5,14 +5,54 @@
  */
 package lista;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 
 
 /**
  *
  * @author João Neto 2
  */
-public class ListaOrdenada extends ListaDuplaCauda {
+public class ListaOrdenada<T> extends ListaDuplaCauda<T> {
      
+public void ordenar(int chave, T dados){
+    Elemento<T> e = new Elemento<T>(chave,dados);
+    String[] companies = { "Yahoo", "Vodafone", "Samsung" };
 
-  
+// sorting java array in ascending order
+System.out.println("Exemplo de Ordenção Crescente em Java");
+System.out.println("Array de String Desordenada: ");
+printNumbers(companies);
+Arrays.sort(companies);
+System.out.println("Array de String Ordenada em Ordem Crescente: ");
+printNumbers(companies);
+
+// sorting java array in descending order
+System.out.println("Exemplo de Ordenção Decrescente em Java ");
+System.out.println("Array de String Desordenada: ");
+printNumbers(companies);
+Arrays.sort(companies, Collections.reverseOrder());
+System.out.println("Array de String Ordenada em Ordem Decrescente: ");
+printNumbers(companies);
+
+System.out.println("Classificando parte do Array em Java:");
+int[] numbers = { 1, 3, 2, 5, 4 };
+Arrays.sort(numbers, 0, 3);
+System.out.println("Sub Array Classificado em Java: ");
+for (int num : numbers) {
+System.out.println(num);
 }
+
+}
+
+public static void printNumbers(String[] companies) {
+for (String company : companies) {
+System.out.println(company);
+}
+}
+
+
+}
+  
+
